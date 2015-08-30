a0183c6ff3b13c19fa64e836e0b321d773a0f246
Yas Okada <yas.okadatech@gmail.com>
2015-08-31 06:28:38 +0900

    layout params builder

---
+++ common/src/androidTest/java/com/okadatech/android/common/view/ParamsBuilderTest.java
@@ -0,0 +1,37 @@
+package com.okadatech.android.common.view;
+
+
+import android.view.ViewGroup;
+
+import junit.framework.TestCase;
+
+public class ParamsBuilderTest extends TestCase {
+
+    public void testViewGroup() {
+        final ViewGroup.LayoutParams params = new ParamsBuilder()
+                .build();
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.width);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+    }
+
+    public void testViewGroupWidthHeight() {
+        final ViewGroup.LayoutParams params = new ParamsBuilder()
+                .width(1)
+                .height(2)
+                .build();
+        assertEquals(1, params.width);
+        assertEquals(2, params.height);
+    }
+
+    public void testViewGroupMargin() {
+        final ViewGroup.MarginLayoutParams params = new ParamsBuilder()
+                .margin(1, 2, 3, 4)
+                .build();
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.width);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+        assertEquals(1, params.leftMargin);
+        assertEquals(2, params.topMargin);
+        assertEquals(3, params.rightMargin);
+        assertEquals(4, params.bottomMargin);
+    }
+}
