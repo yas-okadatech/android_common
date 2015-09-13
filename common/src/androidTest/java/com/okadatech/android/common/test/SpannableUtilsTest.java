04dcf7f300ac0ce601c8b86033aa1d9a7a05ca67
Yas Okada <yas.okadatech@gmail.com>
2015-09-14 07:51:38 +0900

    SpannableUtils#replace

--- common/src/androidTest/java/com/okadatech/android/common/test/SpannableUtilsTest.java
+++ common/src/androidTest/java/com/okadatech/android/common/test/SpannableUtilsTest.java
@@ -1,7 +1,10 @@
 package com.okadatech.android.common.test;
 
 import android.text.Html;
+import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 
 import com.okadatech.android.common.SpannableUtils;
 
@@ -21,5 +24,18 @@ public final class SpannableUtilsTest extends TestCase {
 
         s = new SpannableString(Html.fromHtml("<b> test</b> "));
         assertEquals("test", SpannableUtils.trim(s).toString());
+    }
+
+    public void testReplace() {
+        final Spannable s = (Spannable) Html.fromHtml("abc <b>ghi</b>jkl<b>mno</b>pqr");
+        SpannableUtils.replace(s, StyleSpan.class, new SpannableUtils.Replacer<StyleSpan>() {
+            @Override
+            public Object replace(final StyleSpan oldSpan) {
+                return new UnderlineSpan();
+            }
+        });
+
+        assertEquals(2, s.getSpans(0, s.length(), UnderlineSpan.class).length);
+        assertEquals(0, s.getSpans(0, s.length(), StyleSpan.class).length);
     }
 }
--- common/src/main/java/com/okadatech/android/common/SpannableUtils.java
+++ common/src/main/java/com/okadatech/android/common/SpannableUtils.java
@@ -1,6 +1,9 @@
 package com.okadatech.android.common;
 
+import android.support.annotation.NonNull;
+import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.Spanned;
 
 public final class SpannableUtils {
     private SpannableUtils() {
@@ -20,5 +23,22 @@ public final class SpannableUtils {
             return s;
         }
         return (SpannableString) s.subSequence(start, end + 1);
+    }
+
+    public static <T> void replace(final Spannable s, @NonNull final Class<T> oldSpanClass,
+                                   final Replacer<T> replacer) {
+        final T[] spans = s.getSpans(0, s.length(), oldSpanClass);
+        for (final T span : spans) {
+            final int start = s.getSpanStart(span);
+            final int end = s.getSpanEnd(span);
+            s.removeSpan(span);
+            final Object newSpan = replacer.replace(span);
+            s.setSpan(newSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+    }
+
+    public interface Replacer<T> {
+        @SuppressWarnings("UnusedParameters")
+        Object replace(T oldSpan);
     }
 }
